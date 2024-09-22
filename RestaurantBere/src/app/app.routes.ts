eed638a84311c4ecc7c16008420a93f64cbf2920
Javier Aguilar <jaguilarb3@upao.edu.pe>
2024-09-21 22:29:28 -0500

    feat:Agregando carpetas y configurando el tsconfig

--- RestaurantBere/src/app/app.routes.ts
+++ RestaurantBere/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'reservasion', component: AppComponent },
+    { path: 'reservasion/mesas', component: AppComponent },
+    { path: 'reservasion/mesas/datos', component: AppComponent },
+    { path: 'reservasion/mesas/datos/pago', component: AppComponent },
+    { path: '', redirectTo: 'index', pathMatch: 'full' },
+];
