42b0784fe969562ddc111689c49ed873ce9a6756
Gino Guevara <gino_josue_13@hotmail.com>
2024-09-25 02:32:27 -0500

    feat: Landing-Page Implementado

--- RestaurantBere/src/app/app.component.ts
+++ RestaurantBere/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavegadorComponent } from "./navegador/navegador.component";
+import { CuerpoComponent } from "./cuerpo/cuerpo.component";
+import { PiepaginaComponent } from "./piepagina/piepagina.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavegadorComponent, CuerpoComponent, PiepaginaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- RestaurantBere/src/app/app.routes.ts
+++ RestaurantBere/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { NavegadorComponent } from './navegador/navegador.component';
+import { CuerpoComponent } from './cuerpo/cuerpo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'navbar', component: NavegadorComponent },
+    { path: 'cuerpo', component: CuerpoComponent }
+];
