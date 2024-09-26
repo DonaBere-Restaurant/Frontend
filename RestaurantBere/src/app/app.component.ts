67155e42c1e47bed790914169211f29ac598e495
Gino Guevara <gino_josue_13@hotmail.com>
2024-09-25 21:03:10 -0500

    Feature: Implementacion de Crear Cuenta

--- RestaurantBere/src/app/app.component.ts
+++ RestaurantBere/src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { NavegadorComponent } from "./navegador/navegador.component";
 import { CuerpoComponent } from "./cuerpo/cuerpo.component";
 import { PiepaginaComponent } from "./piepagina/piepagina.component";
+import { CrearcuentaComponent } from "./crearcuenta/crearcuenta.component";
+import { NavcrearcuentaComponent } from "./navcrearcuenta/navcrearcuenta.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavegadorComponent, CuerpoComponent, PiepaginaComponent],
+  imports: [RouterOutlet, NavegadorComponent, CuerpoComponent, PiepaginaComponent, CrearcuentaComponent, NavcrearcuentaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
