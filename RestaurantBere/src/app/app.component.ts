f0d47ef6114ef49c7752a06298ef6abde0c137f4
Gino Guevara <gino_josue_13@hotmail.com>
2024-09-25 22:42:22 -0500

    Feat: Implementacion de la inerface Iniciar Sesion

--- RestaurantBere/src/app/app.component.ts
+++ RestaurantBere/src/app/app.component.ts
@@ -5,11 +5,13 @@ import { CuerpoComponent } from "./cuerpo/cuerpo.component";
 import { PiepaginaComponent } from "./piepagina/piepagina.component";
 import { CrearcuentaComponent } from "./crearcuenta/crearcuenta.component";
 import { NavcrearcuentaComponent } from "./navcrearcuenta/navcrearcuenta.component";
+import { NaviniciarsesionComponent } from './naviniciarsesion/naviniciarsesion.component';
+import { IniciarsesionComponent } from "./iniciarsesion/iniciarsesion.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavegadorComponent, CuerpoComponent, PiepaginaComponent, CrearcuentaComponent, NavcrearcuentaComponent],
+  imports: [RouterOutlet, NavegadorComponent, CuerpoComponent, PiepaginaComponent, CrearcuentaComponent, NavcrearcuentaComponent, NaviniciarsesionComponent, IniciarsesionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
