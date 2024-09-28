8da331a6f8980fa8acba5473a38ee47746c4ea65
Gino Guevara <gino_josue_13@hotmail.com>
2024-09-28 12:26:39 -0500

    feat: Agregando validacion al Iniciar Sesion

--- RestaurantBere/src/app/app.routes.ts
+++ RestaurantBere/src/app/app.routes.ts
@@ -1,5 +1,10 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { NavcrearcuentaComponent } from './navcrearcuenta/navcrearcuenta.component';
+import { CrearcuentaComponent } from './crearcuenta/crearcuenta.component';
+import { NaviniciarsesionComponent } from './naviniciarsesion/naviniciarsesion.component';
+import { IniciarsesionComponent } from './iniciarsesion/iniciarsesion.component';
+import { PiepaginaComponent } from './piepagina/piepagina.component';
 
 export const routes: Routes = [
     { path: 'reservasion', component: AppComponent },
@@ -7,4 +12,9 @@ export const routes: Routes = [
     { path: 'reservasion/mesas/datos', component: AppComponent },
     { path: 'reservasion/mesas/datos/pago', component: AppComponent },
     { path: '', redirectTo: 'index', pathMatch: 'full' },
+    // { path: 'navCrearcuenta', component: NavcrearcuentaComponent},
+    // { path: 'crearcuenta', component: CrearcuentaComponent},
+    // { path: 'naviniciarsesion', component: NaviniciarsesionComponent},
+    // { path: 'iniciarsesion', component: IniciarsesionComponent},
+    // { path: 'piePagina', component: PiepaginaComponent},
 ];
