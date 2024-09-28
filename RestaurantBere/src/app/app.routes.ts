969812aa714d0fbe722d43981563727aacba9a32
jaimePalomino <jpalominoc5@upao.edu.pe>
2024-09-28 13:00:17 -0500

    Corrección de bug: se arreglo el enrutado y se conecto correctamene con el backend

--- RestaurantBere/src/app/app.routes.ts
+++ RestaurantBere/src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { CuerpoComponent } from './cuerpo/cuerpo.component';
+import { CrearcuentaComponent } from './crearcuenta/crearcuenta.component';
 
 export const routes: Routes = [
     { path: 'reservasion', component: AppComponent },
     { path: 'reservasion/mesas', component: AppComponent },
     { path: 'reservasion/mesas/datos', component: AppComponent },
     { path: 'reservasion/mesas/datos/pago', component: AppComponent },
-    { path: '', redirectTo: 'index', pathMatch: 'full' },
+    { path: 'inicio', component: CuerpoComponent },  // Ruta a la página de inicio
+    { path: 'crearcuenta', component: CrearcuentaComponent }, 
+    { path: '', redirectTo: 'inicio', pathMatch: 'full' }, //inicio apunta a cuerpo component
 ];
