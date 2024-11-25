9aa24ef4ae509631c2f4e2253e77214e00c7dc67
RpC05 <jpalominoc5@upao.edu.pe>
2024-11-25 10:20:04 -0500

    feat: adding pdf for reservation

--- RestaurantBere/src/app/pages/reserva/detallesreservabien/detallesreservabien.component.ts
+++ RestaurantBere/src/app/pages/reserva/detallesreservabien/detallesreservabien.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ReservaService } from '../../../core/Services/Reserva/reserva.service';
 
 @Component({
   selector: 'app-detallesreservabien',
@@ -8,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrl: './detallesreservabien.component.scss'
 })
 export class DetallesreservabienComponent {
+  constructor(
+    private reservaService: ReservaService
+  ) {}
 
+  downloadReservationPdf() {
+    this.reservaService.downloadPdf().subscribe({
+      next: (blob) => this.downloadFile(blob, "reporte_mensual.pdf")
+    });
+  }
+
+  private downloadFile(blob: Blob, fileName: string) {
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = fileName;
+    a.click();
+    window.URL.revokeObjectURL(url);
+  }
 }
