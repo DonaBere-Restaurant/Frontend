2d1e72591c36b6d14707bc108d62c7b9cac53a84
RpC05 <jpalominoc5@upao.edu.pe>
2024-11-25 10:01:25 -0500

    fix: adding global components

--- RestaurantBere/src/app/pages/reset-password/advice/advice.component.ts
+++ RestaurantBere/src/app/pages/reset-password/advice/advice.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { PiepaginaComponent } from "../../../shared/components/piepagina/piepagina.component";
+import { NavegadorComponent } from "../../../shared/components/navegador/navegador.component";
 
 @Component({
   selector: 'app-advice',
   standalone: true,
-  imports: [],
+  imports: [PiepaginaComponent, NavegadorComponent],
   templateUrl: './advice.component.html',
   styleUrl: './advice.component.scss'
 })
