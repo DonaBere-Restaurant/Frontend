1d2af03aef6db18e841db9e4f48727efa9b93992
javierUPAO <jaguilarb3@upao.edu.pe>
2025-05-07 00:46:39 -0500

    fix:actualizando url backend

--- RestaurantBere/src/environments/environment.prod.ts
+++ RestaurantBere/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production : true,
-    baseURL : "http://52.14.204.179:8080/api/v1"
+    baseURL : "http://3.14.49.23:8080/api/v1"
 
 };
--- RestaurantBere/src/environments/environment.ts
+++ RestaurantBere/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production : false,
-    baseURL : "http://52.14.204.179:8080/api/v1"
+    baseURL : "http://3.14.49.23:8080/api/v1"
 };
