b027b412f332c8e02f2a704c15bf7bd2820f7f98
javierUPAO <jaguilarb3@upao.edu.pe>
2025-06-12 08:04:52 -0500

    fix:agregando http a la url

--- RestaurantBere/src/environments/environment.prod.ts
+++ RestaurantBere/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production : true,
-    baseURL : "https://main-lb-1088994905.us-east-2.elb.amazonaws.com/api/v1"
+    baseURL : "http://main-lb-1088994905.us-east-2.elb.amazonaws.com/api/v1"
 
 };
--- RestaurantBere/src/environments/environment.ts
+++ RestaurantBere/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production : false,
-    baseURL : "https://main-lb-1088994905.us-east-2.elb.amazonaws.com/api/v1"
+    baseURL : "http://main-lb-1088994905.us-east-2.elb.amazonaws.com/api/v1"
 };
