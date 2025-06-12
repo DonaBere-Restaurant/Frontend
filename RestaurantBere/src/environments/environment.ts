6854fb68f8985c012410a8ba341cb1f263c510b4
javierUPAO <jaguilarb3@upao.edu.pe>
2025-06-12 00:30:58 -0500

    actualizando url backend 2

--- RestaurantBere/src/environments/environment.ts
+++ RestaurantBere/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production : false,
-    baseURL : "https://9d0o67x3yj.execute-api.us-east-2.amazonaws.com/api/v1"
+    baseURL : "https://6ahtmfhzik.execute-api.us-east-2.amazonaws.com/api/v1"
 };
